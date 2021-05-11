1ab4246c58d047238a1ba5806322f1498125f0b1
@beajmnz <github@ainda.net>
2021-05-11 19:22:00 +0200

    Session 3 May 11th

--- theory/03-Data Structures/DS8.py
+++ theory/03-Data Structures/DS8.py
@@ -13,13 +13,23 @@ Write a Python program to find maximum and the minimum value in a set.
 
 values = {16,2,3,45,5,6,72,8,9}
 
+
 maxValue = values.pop()
 minValue = maxValue
 
+# OPTION 1
+
 while len(values) > 0:
-    i = values.pop()
+    i = values.pop()  # It's not a good idea to  modify the iterable within a loop
     if i > maxValue: maxValue = i
     if i < minValue: minValue = i
     
-print("Max:",maxValue,"\nMin:",minValue)
+# OPTION 2
 
+for value in values:
+    if value > maxValue: maxValue = value
+    if value < minValue: minValue = value
+
+
+    
+print("Max:",maxValue,"\nMin:",minValue)
